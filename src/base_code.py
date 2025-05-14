03b19c57b0e5b7c2ea0723de1200a4c2bdb3e20b
Adrian Gould (Lec) <adrian.gould@nmtafe.wa.edu.au>
2025-05-14 14:04:18 +0800

    init: Base Code in src folder

--- src/base_code.py
+++ src/base_code.py
@@ -1,9 +1,28 @@
+"""
+ Assessment Title: Portfolio Part X
+ Cluster:          Intermediate RIoT
+ Qualification:    ICT50220 Diploma of Information Technology (Advanced Programming)
+ Name:             YOUR NAME
+ Student ID:       xxxxxxxxx
+ Year/Semester:    20YY/SN
+
+ YOUR SUMMARY OF PORTFOLIO ACTIVITY
+ GOES HERE
+
+"""
+
+# Imports
 from pathlib import Path
 
 from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
+# Global CONSTANTS
+
+
+## Global Variables
 
 app = FastAPI(title="FastAPI & Jinja2")
 
@@ -26,6 +45,8 @@ app.mount("/images",
 
 TEMPLATES = Jinja2Templates(directory="templates")
 
+
+# Functions/Methods
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return TEMPLATES.TemplateResponse(
@@ -45,3 +66,4 @@ async def about(request: Request):
 async def api_data(request: Request):
     return { 'name':'Frank Spencer' }
 
+# Main Code
